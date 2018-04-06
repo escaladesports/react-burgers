d909e0966b1719458c86b5c650aec42e0a8584de
Kennedy Rose <kennedy@kennedyrose.com>
2018-04-06 09:40:31 -0500

    Vortex reverse

---
+++ src/vortex-reverse.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+import Base from './base'
+import defaultProps from './default-props'
+
+export class VortexReverse extends React.Component{
+	render() {
+		return (
+			<Base className='BurgerVortexReverse' {...this.props}>
+				<style jsx global>{`
+					.BurgerVortexReverse{
+						.BurgerInner{
+							transition-duration: 0.2s;
+							transition-timing-function: cubic-bezier(0.19, 1, 0.22, 1);
+
+							&:before,
+							&:after {
+								transition-duration: 0s;
+								transition-delay: 0.1s;
+								transition-timing-function: linear;
+							}
+							&:before {
+								transition-property: top, opacity;
+							}
+							&:after {
+								transition-property: bottom, transform;
+							}
+						}
+
+						&.BurgerActive {
+							.BurgerInner {
+								transform: rotate(-765deg);
+								transition-timing-function: cubic-bezier(0.19, 1, 0.22, 1);
+
+								&:before,
+								&:after {
+									transition-delay: 0s;
+								}
+								&:before {
+									top: 0;
+									opacity: 0;
+								}
+								&:after {
+									bottom: 0;
+									transform: rotate(-90deg);
+								}
+							}
+						}
+					}
+				`}</style>
+			</Base>
+		)
+	}
+}
+
+VortexReverse.defaultProps = defaultProps
