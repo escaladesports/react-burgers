7b8b960ab4d77e37f10ae9fd319f6b4967a70378
Kennedy Rose <kennedy@kennedyrose.com>
2018-04-06 10:20:07 -0500

    Perspective Y Reverse

--- dev/dev.js
+++ dev/dev.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { PerspectiveXYReverse as Burger } from '../src/index'
+import { PerspectiveYReverse as Burger } from '../src/index'
 
 const containerEl = document.createElement('div')
 document.body.appendChild(containerEl)
